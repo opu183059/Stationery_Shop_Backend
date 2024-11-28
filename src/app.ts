502c77d7e46f10f91985202434b00f7cc18c8266
opu183059 <opuctg440@gmail.com>
2024-11-28 17:32:36 +0600

    Server connection issue solved and connected successfully

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 // app.use('/api/user', userRouter)
 // app.use('/api/tour', tourRouter)
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
-function server() {
+async function server() {
   try {
-    mongoose.connect(config.database_url as string);
-
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port}`);
-    });
+      console.log(`Server running on port ${config.port}`)
+    })
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
-server();
+server()
